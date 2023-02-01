672037d411aac3877a21951d3794856fc0a1b577
root <root@honeypot>
2023-02-01 16:19:38 -0600

    recreated repo for public use

---
+++ README.md
@@ -0,0 +1,25 @@
+# Remote KAPE Script
+
+**Introduction**
+
+This is a PowerShell script that automates the process of performing KAPE triages and memory captures across multiple remote machines at once. The script establishes a remote PowerShell session with the remote machines, and if the PowerShell version on the remote machine is compatible (version 4 or higher), copies a compressed archive of KAPE to the remote machine, extracts it, and starts the capture process. This is all accomplished as a WinRM session background job for each machine. The script then monitors the job status of the captures across all machines, and retrieves the captured data when complete. Data is compressed on the endpoint prior to retrieval.
+
+**Example Use Case:**
+- Capturing baseline forensic data of critical servers or suspicious endpoints at the beginning of an engagement.
+
+**Prerequisites**
+- The computers you want to perform memory captures on must be in the same domain as the machine you are running the script on.
+- The user account running the script must have administrator privileges on the remote machines.
+- The computers.txt file containing a list of the computer names must be located in the deployment_package folder.
+- The KAPE package must be located in the deployment_package folder.
+
+**Usage**
+- Open PowerShell as an administrator on the machine you want to run the script on.
+- Run the script.
+- Enter your credentials when prompted.
+- The script will establish remote PowerShell sessions with the remote machines, copy KAPE to the remote machines, start the captures, and retrieve the captured data when the captures are complete.
+
+**Note**
+- Memory captures are performed via magnet ram capture. Upgrading to WinPMEM in the future
+- The script writes the output of each capture to the remotecapturelog.txt file located in the deployment_package folder for troubleshooting.
+- The script removes the KAPE package and the captured data from the remote machines after the data has been retrieved.
---
+++ remote_capture.ps1
@@ -0,0 +1,46 @@
+﻿Remove-PSSession *
+Remove-Job -name *
+enable-psremoting -skipnetworkprofilecheck -force
+$cred = get-credential
+$user= 'Administrator'
+$computers_file = "C:\Users\$user\Documents\deployment_package\computers.txt"
+$iterative = Get-Content $computers_file
+set-variable -name 'source_dir' -value ("C:\Users\$user\Documents\deployment_package\") -Scope global -PassThru | Out-Null
+
+<#Populate target machines in coomputers.txt using the follwing command:
+get-adcomputer -Filter * -properties dnshostname, enabled, lastlogondate, ipv4address, operatingsystem, operatingsystemservicepack | out-gridview
+Review this listing, select desired endpoints, and review/gain approval from customer prior to execution#>
+
+ForEach ($computer in $iterative) {
+    #$session = New-PSSession -computername $computer -Credential $cred #-ConfigurationName microsoft.powershell32 
+    Write-Host ("Attempting to open WINRM session on $computer")
+    Set-Variable -Name 'session' -value (New-PSSession -name $computer -computername $computer -Credential $cred)
+    $powershellversioncheck = Invoke-Command -Session $session -ScriptBlock {$PSVersionTable.PSVersion}
+    if ($powershellversioncheck.major -ge 4){
+    Write-Host ("WinRM tunnel opened and powershell version compatible. Copying Kape to endpoint")
+    Copy-Item -Path "$source_dir\kape" -Destination ('C:\windows\Temp\') -tosession $session -Recurse -Force}
+    Invoke-Command -Session $session -ScriptBlock {
+    if ((Test-Path -Path 'C:\Windows\Temp\kape') -and (Get-CimInstance -classname win32_logicaldisk -filter "deviceid='C:'" | ?{$_.FreeSpace -ge 9000MB})){
+    $hostname = hostname
+    Expand-Archive -literalpath C:\windows\temp\kape.zip -DestinationPath C:\windows\temp\kape\ -Force
+    start-process -filepath 'C:\windows\temp\kape\kape.exe' -ArgumentList ( "--tsource C: --tdest C:\Windows\Temp\kape\$hostname\ --target !SANS_Triage,Antivirus,CloudStorage_Metadata,CloudStorage_OneDriveExplorer,CombinedLogs,EvidenceOfExecution,Exchange,FileSystem,RegistryHives,RemoteAdmin,ServerTriage,WebServers --zip $hostname --msource C:\windows\temp\KAPE\Modules\bin --mdest C:\windows\temp\KAPE\ --module MagnetForensics_RAMCapture ") -Wait}
+    } -AsJob -JobName "$computer" #comment this line just after the }, to remain inside sessions and recieve output of script for troubleshooting/testing. This will significantly slow the deployment process.
+   Write-Host ("Memory capture started $computer. Looping to the next machine")
+}
+Write-Host ("Monitoring captures. Initiating retievals immediately upon completion")
+while ((Get-job -State Completed -HasMoreData $true) -or (get-job -state Running)) {
+if (Get-job -State Completed -HasMoreData $true ) {
+$job_to_session_link = get-Job -State Completed -HasMoreData $true | select -property name 
+$trimmed_name = $job_to_session_link.Name
+$session = Get-PSSession -Name $trimmed_name
+Write-Host ("Initiating retieval on $trimmed_name")
+Copy-Item -FromSession $session -literalpath C:\windows\Temp\kape\$trimmed_name\*.zip -Destination $source_dir -Recurse -Force
+Copy-Item -FromSession $session -literalpath C:\windows\Temp\kape\$trimmed_name\*.txt -Destination $source_dir -Recurse -Force
+Invoke-Command -Session $session -ScriptBlock {
+remove-item -recurse -force 'C:\Windows\Temp\kape'
+} -AsJob -JobName "cleanup"
+Get-job -Name $trimmed_name | Receive-Job *>&1 >> $source_dir\remotecapturelog.txt}
+}
+
+<#Remove-PSSession *
+Remove-Job -name *#>
